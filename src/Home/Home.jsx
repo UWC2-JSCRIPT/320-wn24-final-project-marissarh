6c7ab1f8adc0092264c00124437c59ecba682fa8
marissarh <mbutto26@gmail.com>
2024-03-04 21:03:39 -0800

    Create Home.jsx

---
+++ src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Home() {
+
+    const [location, setLocation] = useState('');
+    const [weatherData, setWeatherData] = useState(null);
+  return (
+    <div>
+        <h2>Home</h2>
+        </div>
+  )
+}
+
+export default Home
