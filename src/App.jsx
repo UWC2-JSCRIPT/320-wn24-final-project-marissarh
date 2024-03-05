9318acfab1dbd018ddaee998152e9eeaa0daefa9
marissarh <mbutto26@gmail.com>
2024-03-05 15:43:12 -0800

    Add Navbar to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import "./App.css";
+import "./App.css"
+import { Router } from "react-router-dom"
 
+
+
+function Hourly() {
+  return (
+    
+      <h2>Forecast by the Hour</h2>
+  )
+}
+function EightDay() {
+  return (
+    
+      <h2>8 Day Forecast</h2>
+  )
+}
+function Weekend() {
+  return (
+    
+      <h2>Weekend Forecast</h2>
+  )
+}
 function App() {
   return (
-    <>
-      <h1>Final Project</h1>
-    </>
-  );
+    <Router>
+      <div>
+        <Navbar />
+      </div>
+    </Router>
+    
+  )
 }
+
 
 export default App;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
   return (
     <div>
         <h2>Home</h2>
+        <input type/>
         </div>
   )
 }
