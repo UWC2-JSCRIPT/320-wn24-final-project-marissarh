fd8ba7f3085902e398593096ae101c4e1058ea58
marissarh <mbutto26@gmail.com>
2024-03-16 18:15:05 -0700

    PropTypes
    Add PropTypes to the Home, TimeandLocation, and WeatherDisplay component

--- src/TimeandLocation.jsx
+++ src/TimeandLocation.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './TimeandLocation.css'
 import { formatToLocalTime } from './FetchedData/FetchedData'
+import PropTypes from 'prop-types';
 
 function TimeandLocation({weather: {dt, timezone}}) {
   return (
@@ -11,5 +12,11 @@ function TimeandLocation({weather: {dt, timezone}}) {
     </div>
   )
 }
+TimeandLocation.propTypes ={
+  weather: PropTypes.shape({
+    dt: PropTypes.number.isRequired,
+    timezone: PropTypes.string.isRequired
+  }).isRequired
+};
 
 export default TimeandLocation
